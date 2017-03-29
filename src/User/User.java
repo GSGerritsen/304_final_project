1cb1074b23f41774e5fdff23aa8710e56789eea5
Andrew <andrewchang17@hotmail.com>
2017-03-28 19:57:38 -0700

    User & admin

--- src/User/User.java
+++ src/User/User.java
@@ -3,11 +3,13 @@ package User;
 
 public class User {
 
-
+    private int uid;
     private String username;
     private String email;
     private boolean ban;
     private boolean admin;
+
+    public int getUid() { return uid; }
 
     public String getUsername() {
         return username;
@@ -17,16 +19,27 @@ public class User {
         return email;
     }
 
-    public boolean getBan() {
-        return ban;
-    }
+    public boolean getBan() { return ban; }
+    private void setBan() { ban = true; }
+    private void unBan() { ban = false; }
 
     public boolean getAdmin() { return admin; }
 
-    public User(String username, String email, boolean ban, boolean admin) {
+    public User(int uid, String username, String email, boolean ban, boolean admin) {
+        this.uid = uid;
         this.username = username;
         this.email = email;
         this.ban = ban;
         this.admin = admin;
+    }
+
+
+    public void banUser(int uid, User user) {
+        if (admin && !user.getAdmin() && user.getUid() == uid)
+            user.setBan();
+    }
+    public void unbanUser(int uid, User user) {
+        if (admin && !user.getAdmin() && user.getUid() == uid)
+            user.unBan();
     }
 }
